cc58a461cbcc62561082ab4f974a32acb502e4eb
SavionLegendZzz <saviondeavault@gmail.com>
2016-07-10 13:40:11 -0400

    :+1:

--- src/arrow/Main.php
+++ src/arrow/Main.php
@@ -3,26 +3,35 @@
 namespace arrow;
 
 use pocketmine\event\Listener;
+
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\entity\ProjectileHitEvent;
+
 use pocketmine\Player;
+use pocketmine\Server;
+
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
-use pocketmine\event\entity\EntityDamageByChildEntityEvent;
-use pocketmine\utils\TextFormat;
+
+use pocketmine\utils\TextFormat as TF;
 
 
 class Main extends PluginBase Implements Listener{
-    
-    public function onEnable(){
-        $this->getServer()->getPluginManager()->registerEvents($this,$this);
-    }
 
-public function onHit(EntityDamageEvent $event){
-        if($event instanceof EntityDamageByChildEntityEvent){
-            $victim = $event->getEntity();
-            $shooter = $event->getDamager();
-                $shooter->sendMessage("§l§cPlayer§9 ".$victim->getName()." §cHas§9 ".$victim->getHealth()."/".$victim->getMaxHealth()." §cHearts left");
-           }
-      }
- }
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this,$this);
+		$this->getServer()->getLogger()->info(TF::GREEN."Enabled!");
+	}
+
+	public function onDisable(){
+		$this->getServer()->getLogger()->info(TF::RED."Disabled!");
+	}
+
+	public function onHit(EntityDamageEvent $ev){
+		$player = $ev->getEntity();
+		$cause = $player->getLastDamageCause();
+		if($cause instanceof EntityDamageByEntityEvent && $ev->getCause() === EntityDamageEvent::CAUSE_PROJECTILE){
+			$damager = $cause->getDamager();
+			$damager->sendMessage(TF::GOLD.$player->getName().TF::DARK_PURPLE." has ".TF::RED.$player->getHealth().TF::DARK_PURPLE." <3's left!");
+		}
+	}
+}
